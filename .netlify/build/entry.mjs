407b5381db2bcce22e7807094efc8ac5ff587673
Gary Bleasdale <gary@bleasdale.dev>
2025-05-03 06:24:45 -0300

    (bug) fix typescript error

---
+++ .netlify/build/entry.mjs
@@ -0,0 +1,36 @@
+import { renderers } from './renderers.mjs';
+import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
+import { manifest } from './manifest_Gq4LSUVa.mjs';
+import { createExports } from '@astrojs/netlify/ssr-function.js';
+
+const serverIslandMap = new Map();;
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/api/commits/_year_.json.astro.mjs');
+const _page2 = () => import('./pages/git.astro.mjs');
+const _page3 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/api/commits/[year].json.ts", _page1],
+    ["src/pages/git.astro", _page2],
+    ["src/pages/index.astro", _page3]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    actions: () => import('./_noop-actions.mjs'),
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "ad4550e8-6fd9-4f02-80e4-15b285190d19"
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (_start in serverEntrypointModule) {
+	serverEntrypointModule[_start](_manifest, _args);
+}
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
